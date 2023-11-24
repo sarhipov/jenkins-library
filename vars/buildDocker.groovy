186b87dfa759b072f4d482e15f285a5b843af627
Sergei Arhipov <sergei.arhipov@topia.com>
2023-11-24 14:11:53 +0200

    add README.md fix to Dockerfiles

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,15 +1,23 @@
+/// buildDocker.groovy
 def call(Map params = [:]) {
-    stage('Build Docker Image') {
-        steps {
-            script {
-                try {
-                    docker.withRegistry('', params.dockerRegistryCredentialsId) {
-                        def dockerImage = docker.build(params.dockerImageName, "-f Dockerfile .")
-                        dockerImage.push()
+    pipeline {
+        agent {
+            label params.agentLabel ?: 'any'
+        }
+        stages {
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        try {
+                            docker.withRegistry('', params.dockerRegistryCredentialsId) {
+                                def dockerImage = docker.build(params.dockerImageName, "-f Dockerfile .")
+                                dockerImage.push()
+                            }
+                        } catch (Exception e) {
+                            echo "Error in Build Docker Image stage: ${e}"
+                            currentBuild.result = 'FAILURE'
+                        }
                     }
-                } catch (Exception e) {
-                    echo "Error in Build Docker Image stage: ${e}"
-                    currentBuild.result = 'FAILURE'
                 }
             }
         }
--- vars/k8sDeploy.groovy
+++ vars/k8sDeploy.groovy
@@ -1,18 +1,26 @@
+/// k8sDeploy.groovy
 def call(Map params = [:]) {
-    stage('Deploy to Kubernetes') {
-        steps {
-            script {
-                try {
-                    def k8sDeployment = readFile('deployment.yaml')
-                    k8sDeployment = k8sDeployment.replaceAll('\\$KUBERNETES_DEPLOYMENT_NAME', params.kubernetesDeploymentName)
-                    k8sDeployment = k8sDeployment.replaceAll('\\$DOCKER_IMAGE_NAME', params.dockerImageName)
+    pipeline {
+        agent {
+            label params.agentLabel ?: 'any'
+        }
+        stages {
+            stage('Deploy to Kubernetes') {
+                steps {
+                    script {
+                        try {
+                            def k8sDeployment = readFile('deployment.yaml')
+                            k8sDeployment = k8sDeployment.replaceAll('\\$KUBERNETES_DEPLOYMENT_NAME', params.kubernetesDeploymentName)
+                            k8sDeployment = k8sDeployment.replaceAll('\\$DOCKER_IMAGE_NAME', params.dockerImageName)
 
-                    withCredentials([file(credentialsId: params.kubeconfigCredentialsId, variable: 'KUBECONFIG')]) {
-                        sh "KUBECONFIG=$KUBECONFIG echo '${k8sDeployment}' | kubectl apply -f -"
+                            withCredentials([file(credentialsId: params.kubeconfigCredentialsId, variable: 'KUBECONFIG')]) {
+                                sh "KUBECONFIG=$KUBECONFIG echo '${k8sDeployment}' | kubectl apply -f -"
+                            }
+                        } catch (Exception e) {
+                            echo "Error in Deploy to Kubernetes stage: ${e}"
+                            currentBuild.result = 'FAILURE'
+                        }
                     }
-                } catch (Exception e) {
-                    echo "Error in Deploy to Kubernetes stage: ${e}"
-                    currentBuild.result = 'FAILURE'
                 }
             }
         }
